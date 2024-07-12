358266e77f74af41348563887430da1f934e9778
Monteg1 <65545981+Monteg1@users.noreply.github.com>
2024-07-12 22:51:50 +0300

    Create .eslintrc.cjs

---
+++ .eslintrc.cjs
@@ -0,0 +1,31 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  plugins: [
+    'react',
+    '@typescript-eslint',
+  ],
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+    jest: true,
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  rules: {
+    'react/prop-types': 'off',
+    'no-unused-vars': 'warn',
+    'no-console': 'warn',
+    'react/react-in-jsx-scope': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'warn',
+  },
+};
